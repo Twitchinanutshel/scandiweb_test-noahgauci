09fcaa16265457e97fd21459e1284363eddd71bb
Noah Gauci <breakf117@gmail.com>
2025-05-10 19:27:47 +1000

    added base: / to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-    rollupOptions: {
-      input: 'frontend/dist/index.html', 
-    },
-  },
+  base: '/'
 })
