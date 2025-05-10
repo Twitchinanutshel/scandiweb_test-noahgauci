a167124671e0eaf4e7c9f65509a3327186230e99
Noah Gauci <breakf117@gmail.com>
2025-05-10 20:06:01 +1000

    changed url

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://scandiweb-test-noahgauci-5b174efbd7da.herokuapp.com',
+    uri: 'https://scandiweb-test-noahgauci-5b174efbd7da.herokuapp.com/graphql',
     credentials: 'omit',
     headers: {
       'Content-Type': 'application/json',
